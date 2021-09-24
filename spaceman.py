65065d96b6ed0115c64aa3baf18c798c4fdd5444
George Ceja <Net.george.sky@gmail.com>
2021-09-24 03:57:36 -0700

    add some style

--- spaceman.py
+++ spaceman.py
@@ -1,8 +1,7 @@
 from random import choice
+from os import system
 
-YELLOW = '\033[93m'
-RED = '\033[91m'
-ENDC = '\033[0m'
+YELLOW, RED, ENDC = '\033[93m', '\033[91m', '\033[0m'
 
 def get_word_list():
     with open("words.txt") as file:
@@ -10,29 +9,35 @@ def get_word_list():
         file.close()
         return words
 
-
 def start_spaceman():
+    system('clear')
+    guesses_left = 7
+    dash = "-" * 110
     words = get_word_list()
     correct_word = choice(words)
     word_left = list(correct_word)
     correct_word_len = len(correct_word)
     hidden_word = list("_" * correct_word_len)
     letters_not_guessed = list("abcdefghijklmnopqrstuvwxyz")
-    dash = "-" * 100
-    guesses_left = 7
 
-    print("Welcome to Spaceman!")
-    print("The secret word contains 5 letters")
-    print("You have 7 incorect guesses, please enter only one letter")
-    print(dash)
+    print("""
+          Welcome to Spaceman!
+          The secret word contains 5 letters
+          You have 7 incorect guesses, please enter only one letter
+          """)
 
     while True:
-        guess = input("Enter a letter: ").lower()
-        if not guess.isalpha() or guess not in letters_not_guessed:
-            print("Only letters allowed! Can't guess same letter")
-            continue
+        print(f"""{dash}
+        Your have {guesses_left} guesses left
+        {YELLOW}Letters not guessed yet: {', '.join(letters_not_guessed) + ENDC}
+        Guessed word so far: {''.join(hidden_word)}
+        """)
 
-        print(f"correct word: {correct_word}")
+        guess = input("Enter a letter: ").lower()
+        system('clear')
+        if not guess.isalpha() or guess not in letters_not_guessed:
+            print(f"{RED}Only only letter is allowed! Can't guess same letter{ENDC}")
+            continue
 
         letters_not_guessed.remove(guess)
         occurences = word_left.count(guess)
@@ -43,21 +48,18 @@ def start_spaceman():
                 hidden_word[index] = guess
                 word_left[index] = "-"
                 print("Your guess appears in the word!")
-                print(f"Guessed word so far: {''.join(hidden_word)}")
-                print(f"Letters not guessed yet: {', '.join(letters_not_guessed)}")
-                print(dash)
         else:
             guesses_left -= 1
-            print("Sorry your guess was not in the word, try again")
-            print(f"Your have {guesses_left} guesses left")
+            print(f"{RED}Sorry your guess was not in the word, try again{ENDC}")
+
+            if guesses_left == 0:
+                print(f"{RED}You Lost................{ENDC}")
+                print(f"The correct word was: {YELLOW + correct_word + ENDC}")
+                break
             continue
 
         if correct_word == "".join(hidden_word):
             print(f"Nice you guessed the word: {correct_word} with {guesses_left} attempts left!")
-            break
-
-        elif guesses_left < 1:
-            print("You Lost..............")
             break
 
 start_spaceman()
